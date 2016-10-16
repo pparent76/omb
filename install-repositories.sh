edb14fc0d1a7cf5aa9988da2dc731bdf1f47b44b
Dashamir Hoxha <dashohoxha@gmail.com>
2016-10-16 23:09:50 +0200

    Use repositories from https://github.com/Own-Mailbox/

--- install-repositories.sh
+++ install-repositories.sh
@@ -1,22 +1,18 @@
 #!/bin/bash -xe
 
 cd /root/
-git clone https://github.com/pparent76/Omb-ihm.git
-cd Omb-ihm
-# Get specific revision in order to have reproducible results
-git reset --hard e2943286bbb10525ebfaf203f9d3e2b5ad7a154b
+git clone --branch alpha https://github.com/Own-Mailbox/ihm
+cd ihm
 make
 
 cd /root/
-git clone https://github.com/pparent76/Omb-cs-com.git
-cd Omb-cs-com/client/
+git clone https://github.com/Own-Mailbox/cs-com
+cd cs-com/client/
 # Get specific revision in order to have reproducible results
 git reset --hard 664e581912222c9715bc2c9fc9219138527f5d0e
 make && make install
 
 cd /root/
-git clone https://github.com/pparent76/ttdnsd.git
+git clone --branch alpha https://github.com/Own-Mailbox/ttdnsd
 cd ttdnsd
-# Get specific revision in order to have reproducible results
-git reset --hard b9be844df3b6e061a37f00f431c65c0416091730
 make install
