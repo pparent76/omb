89be398762f639d7264002825cb17ee295f2f045
Dashamir Hoxha <dashohoxha@gmail.com>
2016-10-18 03:12:22 +0200

    Get the branch alpha from cs-com.

--- install-repositories.sh
+++ install-repositories.sh
@@ -6,10 +6,8 @@ cd ihm
 make
 
 cd /root/
-git clone https://github.com/Own-Mailbox/cs-com
+git clone --branch alpha https://github.com/Own-Mailbox/cs-com
 cd cs-com/client/
-# Get specific revision in order to have reproducible results
-git reset --hard 664e581912222c9715bc2c9fc9219138527f5d0e
 make && make install
 
 cd /root/
