82870740a1f6a4c8bfd6dba3d2df9de4ac7f8227
Dashamir Hoxha <dashohoxha@gmail.com>
2018-03-14 05:50:53 +0100

    Fix cmd_create

--- cmd/create.sh
+++ cmd/create.sh
@@ -10,8 +10,8 @@ rename_function cmd_create orig_cmd_create
 cmd_create() {
     orig_cmd_create \
         --cap-add ALL --privileged \
-        --mount type=bind,src=$(realpath $APP_DIR),dst=/opt/Own-Mailbox/docker \
-        --env APP_DIR=/opt/Own-Mailbox/docker \
+        --mount type=bind,src=$(realpath $APP_DIR),dst=/opt/Own-Mailbox/omb \
+        --env APP_DIR=/opt/Own-Mailbox/omb \
         --workdir /var/www \
         "$@"    # accept additional options, e.g.: -p 2201:22
 
