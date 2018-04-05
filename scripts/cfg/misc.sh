cfb8213f98c1e0b59ba40bcbcf115adbca3c0f50
Pierre Parent <pierre.parent@pparent.fr>
2018-04-05 20:35:56 +0200

    Correction for tor log file

--- scripts/cfg/misc.sh
+++ scripts/cfg/misc.sh
@@ -12,7 +12,7 @@ mkdir /etc/omb/
 chown www-data /etc/omb/
 echo "nameserver 127.0.0.1" > /etc/resolv.conf.head
 
-cp files/torrc /etc/tor/torrc
+cp src/tor/torrc /etc/tor/torrc
 touch /var/log/tor.log
 chown tor /var/log/tor.log
 
